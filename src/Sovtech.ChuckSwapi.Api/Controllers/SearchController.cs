95cebe58c2cbee805874af0e6b0dda20e0b98b2b
eskye <sunkanmiijatuyi@gmail.com>
2022-05-06 10:23:57 +0100

    fix nullable check and error

--- src/Sovtech.ChuckSwapi.Api/Controllers/SearchController.cs
+++ src/Sovtech.ChuckSwapi.Api/Controllers/SearchController.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using Sovtech.ChuckSwap.Api;
 
 namespace Sovtech.ChuckSwapi.Api.Controllers
 {
@@ -7,29 +6,6 @@ namespace Sovtech.ChuckSwapi.Api.Controllers
     [Route("[controller]")]
     public class SearchController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
-        private readonly ILogger<ChuckController> _logger;
-
-        public SearchController(ILogger<ChuckController> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet("search")]
-        public IActionResult Get()
-        {
-            var values = Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-            return Ok(values);
-        }
+        
     }
 }
